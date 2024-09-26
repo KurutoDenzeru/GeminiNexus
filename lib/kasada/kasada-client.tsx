62f5dab2a0c0305bac24c3eb236501e790f3293e
kurutodenzeru <kurtdenzel51@gmail.com>
2024-09-26 13:57:19 +0800

    refactor: fixed kasada

--- lib/kasada/kasada-client.tsx
+++ lib/kasada/kasada-client.tsx
@@ -1,12 +1,11 @@
-import Script from 'next/script'
+import Script from "next/script";
 
 export function KasadaClient() {
-  return (
-    <>
-      <script
-        dangerouslySetInnerHTML={{
-          __html:
-            `document.addEventListener('kpsdk-load', () => {window.KPSDK.configure([
+	return (
+		<>
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `document.addEventListener('kpsdk-load', () => {window.KPSDK.configure([
           {
             domain: location.host,
             path: '/',
@@ -18,13 +17,13 @@ export function KasadaClient() {
             method: 'POST'
           },
         ]);
-    });`.replace(/[\n\r\s]/g, '')
-        }}
-      ></script>
-      <Script
-        async={true}
-        src="/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/p.js"
-      ></Script>
-    </>
-  )
+    });`.replace(/[\n\r\s]/g, ""),
+				}}
+			/>
+			<Script
+				async={true}
+				src="/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/p.js"
+			/>
+		</>
+	);
 }
